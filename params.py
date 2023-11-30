f79d85ad4f63157eb468b17d2a30fb2ef37c635c
MD SHADAB ALAM <88769183+Shaadalam9@users.noreply.github.com>
2023-11-30 12:03:23 +0100

    Update params.py

--- params.py
+++ params.py
@@ -1,7 +1,8 @@
 y_tube_link = "https://www.youtube.com/watch?v=q21Kj-pxJW4"
-resolution = "2160p"
+resolution = "144p"
 output_path = "."
+trim_start = 20 #seconds
 
-model = "yolov8x.pt"
+model = "yolov8x.pt" 
 confidence = 0.4
 render =True
