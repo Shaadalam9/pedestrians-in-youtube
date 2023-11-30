35b6cab563cb43d529281342c96aa65493bb5e1d
MD SHADAB ALAM <88769183+Shaadalam9@users.noreply.github.com>
2023-11-30 12:04:18 +0100

    Update main.py
    Included trimming of video

--- main.py
+++ main.py
@@ -4,12 +4,11 @@
 #   3. speed of crossing
 #   4. hesitation (tbd)
 
-
-import pandas as pd
-import numpy as np
 import params as params
 from pytube import YouTube
 from ultralytics import YOLO
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import os
 
 def download_video_with_resolution(youtube_url, resolution="720p", output_path="."):
     try:
@@ -33,6 +32,11 @@ def download_video_with_resolution(youtube_url, resolution="720p", output_path="
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+    
+def trim_video(input_path, output_path, start_time, end_time):
+    video_clip = VideoFileClip(input_path).subclip(start_time, end_time)
+    video_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+    video_clip.close()
 
 
 result = download_video_with_resolution(params.y_tube_link, resolution=params.resolution, output_path=params.output_path)
@@ -44,11 +48,14 @@ if result:
 else:
     print("Download failed.")
 
+input_video_path = f"{video_title}_{params.resolution}.mp4"
+output_video_path = f"{video_title}_{params.resolution}_mod.mp4"
+start_time = 20  # seconds
+end_time = None  # Set to None to keep the rest of the video
+trim_video(input_video_path, output_video_path, start_time, end_time)
+
 
 
 model = YOLO(params.model)
 print(f"{video_title}_{params.resolution}")
-model.predict(source = f"{video_title}_{params.resolution}.mp4", save= True, conf= params.confidence, save_txt=False, show=params.render)
-
-
-
+model.predict(source = f"{video_title}_{params.resolution}_mod.mp4", save= True, conf= params.confidence, save_txt=False, show=params.render)
