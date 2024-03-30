8e89c8b7d25c55b2f2713d44757e73fe82760d04
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2024-03-30 14:29:11 +0100

    Cleanup

--- download_videos.py
+++ download_videos.py
@@ -1,3 +1,4 @@
+# by Shadab Alam and Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
 import os
 from pytube import YouTube
 import pandas as pd
@@ -11,51 +12,48 @@ logger = CustomLogger(__name__)  # use custom logger
 
 
 def download_video_with_resolution(video_ids, resolutions=["2160p", "1440p", "1080p", "720p", "480p", "360p"],
-                                   output_path="."):
-    try:
-        video_id = [id.strip() for id in video_ids.strip("[]").split(',')]
-        for vid in video_id:
-            # Check if the MP4 file already exists in the output folder
-            mp4_file_path = os.path.join(output_path, f"{vid}.mp4")
-            if os.path.exists(mp4_file_path):
-                logger.debug(f"MP4 file for video with {vid} already exists in the folder.")
-                continue
-            youtube_url = f'https://www.youtube.com/watch?v={vid}'
-            youtube_object = YouTube(youtube_url)
-            for resolution in resolutions:
-                video_streams = youtube_object.streams.filter(res=f"{resolution}") 
-                if video_streams:
-                    logger.debug(f"Found video {vid} in {resolution}.")
-                    break
-
-            if not video_streams:
-                logger.debug(f"No {resolution} resolution available for {vid}.") 
-                return None
-
-            selected_stream = video_streams[0]
-            # Comment the below line to automatically download with video in "video" folder 
-            logger.info("Started download of video {} entitled \"{}\" in resolution {}.", vid, youtube_object.title,
-                        resolution)
-            selected_stream.download(output_path, filename=f"{vid}.mp4") 
-    except Exception as e:
-        logger.error("Error occurred {}.", e)
-        return None
+                                   source_videos="."):
+    video_ids = [id.strip() for id in video_ids.strip("[]").split(',')]
+    for vid in video_ids:
+        # Check if the MP4 file already exists in the output folder
+        mp4_file_path = os.path.join(source_videos, f"{vid}.mp4")
+        if os.path.exists(mp4_file_path):
+            logger.debug(f"MP4 file for video with {vid} already exists in the folder.")
+            continue
+        youtube_url = f'https://www.youtube.com/watch?v={vid}'
+        youtube_object = YouTube(youtube_url)
+        for resolution in resolutions:
+            video_streams = youtube_object.streams.filter(res=f"{resolution}") 
+            if video_streams:
+                logger.debug(f"Found video {vid} in {resolution}.")
+                break
+        if not video_streams:
+            logger.debug(f"No {resolution} resolution available for {vid}.") 
+            return None
+        selected_stream = video_streams[0]
+        logger.info("Started download of video {} entitled \"{}\" in resolution {}.", vid, youtube_object.title,
+                    resolution)
+        # Download
+        try:
+            selected_stream.download(source_videos, filename=f"{vid}.mp4") 
+        except Exception as e:
+            logger.error("Error occurred {}", e)
+    return len(video_ids)  # return number of videos processed
 
 
 if __name__ == "__main__":
-    logger.debug("Download of videos started.")
+    logger.info("Download of videos started.")
     df_mapping = pd.read_csv("mapping.csv")
-
-    new_directory = common.get_configs('source_videos')
-    if not os.path.exists(new_directory):
-        # If it doesn't exist, create it
-        os.makedirs(new_directory)
-        logger.debug("Directory '{}' created successfully.".format(new_directory))
+    source_videos = common.get_configs('source_videos')  # folder for output of videos
+    # Create folder for output
+    if not os.path.exists(source_videos):
+        os.makedirs(source_videos)
+        logger.debug("Directory '{}' created successfully.".format(source_videos))
     else:
-        logger.debug("Directory '{}' already exists.".format(new_directory))
-
+        logger.debug("Directory '{}' already exists.".format(source_videos))
+    num_videos = 0  # counter of videos
+    # Go over videos
     for index, row in df_mapping.iterrows():
-        video_link = row['videos']
-        my_city = row['city']
-        my_condition = row['time_of_day']
-        download_video_with_resolution(video_link, output_path=new_directory)
+        logger.info("Analysing videos for town={}, time_of_day={}.", row['city'], row['time_of_day'])
+        num_videos += download_video_with_resolution(row['videos'], source_videos=source_videos)
+    logger.info("Processed {} videos.", num_videos)
