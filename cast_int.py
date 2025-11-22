de6fe83bda922a82f14d4bc0e75d57092296e710
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2025-11-22 13:51:49 +0100

    FRA

---
+++ cast_int.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("mapping.csv")
+
+
+def clean_to_int(x):
+    if pd.isna(x):
+        return None
+    try:
+        # works for "67391582.0", 67391582.0, "67391582"
+        return int(float(x))
+    except:
+        return None
+
+
+df["population_country"] = df["population_country"].apply(clean_to_int)
+
+df.to_csv("mapping.csv", index=False)
+print("population_country column cleaned and cast to int.")
--- remove_offline_videos.py
+++ remove_offline_videos.py
@@ -161,7 +161,7 @@ output_csv_file = 'mapping_cleaned.csv'
 
 # List of video IDs to remove
 videos_to_delete = [
-    'fkNVYlA0aTk', 'KT4tXVgJRuA'
+    'eyUSkQtVCqI'
 ]
 
 # Run the script
