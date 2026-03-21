b47916005be6aac5edd36219caa4b9b4ed250ed4
Pavlo Bazilinskyy <pavlo.bazilinskyy@gmail.com>
2026-03-21 21:11:37 +0200

    Population as int

--- cast_int.py
+++ cast_int.py
@@ -1,19 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv("mapping.csv")
+df = pd.read_csv('mapping.csv')
 
+df['population_locality'] = df['population_locality'].astype('Int64')
+df['population_country'] = df['population_country'].astype('Int64')
 
-def clean_to_int(x):
-    if pd.isna(x):
-        return None
-    try:
-        # works for "67391582.0", 67391582.0, "67391582"
-        return int(float(x))
-    except:  # noqa: E722
-        return None
-
-
-df["population_country"] = df["population_country"].apply(clean_to_int)
-
-df.to_csv("mapping.csv", index=False)
-print("population_country column cleaned and cast to int.")
+df.to_csv('mapping.csv', index=False)
